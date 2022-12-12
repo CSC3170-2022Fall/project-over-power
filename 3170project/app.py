3701b9a7aa6bd1213b05bbadd080fafdfdfb114b
bizu2001 <120090643@link.cuhk.edu.cn>
2022-12-12 23:19:27 +0800

    finished login(needs further modification)

--- 3170project/app.py
+++ 3170project/app.py
@@ -20,7 +20,7 @@ class common_user(db.Model):#datapage model, inherited from db.Model
 class senior_user(db.Model):#senior user is in charge of the restaurant
     __tablename__="senior_user"
     senior_id=db.Column(db.Integer,primary_key=True)
-    common_name=db.Column(db.String(32),unique=True,nullable=False)
+    senior_name=db.Column(db.String(32),unique=True,nullable=False)
     res_id=db.Column(db.Integer, db.ForeignKey('namelist.list_id'))
 
 class restaurants(db.Model):
@@ -33,6 +33,16 @@ class restaurants(db.Model):
     average_price_per_person = db.Column(db.Float,primary_key=True)
     rate = db.Column(db.Float,primary_key=True)
 
+class dishes(db.Model):
+    __tablename__="dishes"
+    list_id = db.Column(db.Integer, primary_key=True)
+    list_name = db.Column(db.String(40), primary_key=True)
+    info_type = db.Column(db.String(16))
+    info_price = db.Column(db.Integer)
+    info_taste = db.Column(db.String(16))
+    info_times = db.Column(db.Integer)
+
+#needs further adjustment:other link for senior user to login
 @app.route('/', methods=['GET', 'POST'])
 def common_register():
     if request.method=="POST":
@@ -59,6 +69,38 @@ def common_register():
         else:
             return redirect(url_for('info'))
     return render_template("login.html")
+
+
+@app.route('/senior_register', methods=['GET', 'POST'])
+def senior_register():
+    if request.method=="POST":
+        username=request.form.get('username')
+        password=request.form.get('password')
+        check1=senior_user.query.all()#read from common user
+        checka=0
+        checkb=0
+        for i in check1:
+            if i.senior_name==username:#check whether the user exists
+                checka=1
+                if i.senior_password==password:
+                    checkb=1
+                else:
+                    checkb=0
+            else:
+                checka=0
+            if checka==1:
+                break
+        if checka != 1:
+            flash(u'the account does not exist!')
+        elif checkb != 1:
+            flash(u"wrong password or user name!")
+        else:
+            #needs modification: leads to the users' corresponding restaurant
+            return redirect(url_for('info'))
+    #this line may be modified to return render_template("senior_login.html")
+    return render_template("login.html")
+
+
 
 
 
