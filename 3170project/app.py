75e612158c30f1e23d78824e038a148759d68d84
bizu2001 <120090643@link.cuhk.edu.cn>
2022-12-18 21:18:31 +0800

    akira

--- 3170project/app.py
+++ 3170project/app.py
@@ -59,13 +59,6 @@ class comment(db.Model):
     Comment_Time = db.Column(db.String(48), unique=True, nullable=False)#using the system time, you should import "datetime" and "time"
     content = db.Column(db.String(500))#free edit
 
-class comment(db.Model):
-    __tablename__ = "comment"
-    restaurant_id = db.Column(db.Integer, primary_key=True)
-    dish_name = db.Column(db.String(32), primary_key=True)
-    common_id = db.Column(db.Integer, primary_key=True)
-    Comment_Time = db.Column(db.String(48), unique=True, nullable=False)#using the system time, you should import "datetime" and "time"
-    content = db.Column(db.String(500))#free edit
 #needs further adjustment:other link for senior user to login
 @app.route('/', methods=['GET', 'POST'])
 def common_register():
@@ -92,7 +85,7 @@ def common_register():
             flash(u"wrong password or user name!")
         else:
             return redirect(url_for('info'))
-    return render_template("login.html")
+    return render_template("normal_login.html")
 
 @app.route('/senior_register', methods=['GET', 'POST'])
 def senior_register():
@@ -121,4 +114,8 @@ def senior_register():
             #needs modification: leads to the users' corresponding restaurant
             return redirect(url_for('info'))
     #this line may be modified to return render_template("senior_login.html")
-    return render_template("login.html")
+    return render_template("senior_login.html")
+
+
+if __name__ == '__main__':
+    app.run()
